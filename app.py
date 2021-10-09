4985ab21af98e9ed2e710c33f4cfdc4ecd2f898a
Vagrant User <vagrant@multicloud>
2021-10-09 14:27:00 -0300

    adicioando novo endpoint soma

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def pagina_inicial():
     return "Hello World"
 
+@app.route("/soma")
+def soma_valores():
+    return f"Sua soma de 10+10={10+10}"
+
 if __name__ == '__main__':
     app.run()
