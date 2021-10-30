752defe25c1a9db4fdc6e2965b8324f8e389ff2f
Rafael Lino <rafael.veloso.lino@hotmail.com>
2021-10-30 09:52:01 -0300

    ajuste requirements, app e gitignore

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def pagina_inicial():
     return "Hello World"
 
+
 @app.route("/soma")
 def soma_valores():
-    return f"Sua soma de 10+10={10+10}"
+    return f"Sua soma de 10+10={10 + 10}"
+
 
 if __name__ == '__main__':
     app.run()
